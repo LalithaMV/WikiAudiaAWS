cd142a5a707ed72e9d5309d51d3b6003cb25e18e
Ashwini Venkatesh <ashuven63@gmail.com>
2014-02-09 10:21:37 +0530

    Added Interface for uploading a book into the server

--- wa/urls.py
+++ wa/urls.py
@@ -3,5 +3,6 @@ from wa import views;
 
 urlpatterns = patterns('',
     url(r'^$', views.audio, name='audio'),
-    url(r'^audioUpload/$', views.audioUpload, name='audioUpload')
+    url(r'^audioUpload/$', views.audioUpload, name='audioUpload'),
+    url(r'^uploadBook/$', views.uploadBook, name='uploadBook')
 )
